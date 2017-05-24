0292ede486a3f989ebbed4baeca115abe460901a
Nicolas Berger <nicoberger@gmail.com>
2017-05-23 22:53:22 -0700

    Extract views namespace
    It will make it easier to create devcards of just some views

---
+++ src/flappy_bird_demo/views.cljs
@@ -0,0 +1,157 @@
+(ns flappy-bird-demo.views
+  (:require
+   [cljsjs.react]
+   [cljsjs.react.dom]
+   [sablono.core :as sab :include-macros true]))
+
+(defn floor [x] (.floor js/Math x))
+
+(defn translate [start-pos vel time]
+  (floor (+ start-pos (* time vel))))
+
+(def horiz-vel -0.15)
+(def gravity 0.05)
+(def jump-vel 21)
+(def start-y 312)
+(def bottom-y 561)
+(def flappy-x 212)
+(def flappy-width 57)
+(def flappy-height 41)
+(def pillar-spacing 324)
+(def pillar-gap 50)
+(def pillar-width 86)
+
+(def starting-state { :timer-running false
+                      :jump-count 0
+                      :initial-vel 0
+                      :start-time 0
+                      :flappy-start-time 0
+                      :flappy-y   start-y
+                      :pillar-list
+                      [{ :start-time 0
+                         :pos-x 900
+                         :cur-x 900
+                         :gap-top 200 }]})
+
+(defn reset-state [_ cur-time]
+  (-> starting-state
+      (update-in [:pillar-list] (fn [pls] (map #(assoc % :start-time cur-time) pls)))
+      (assoc
+          :start-time cur-time
+          :flappy-start-time cur-time
+          :timer-running true)))
+
+(defn curr-pillar-pos [cur-time {:keys [pos-x start-time] }]
+  (translate pos-x horiz-vel (- cur-time start-time)))
+
+(defn in-pillar? [{:keys [cur-x]}]
+  (and (>= (+ flappy-x flappy-width)
+           cur-x)
+       (< flappy-x (+ cur-x pillar-width))))
+
+(defn in-pillar-gap? [{:keys [flappy-y]} {:keys [gap-top]}]
+  (and (< gap-top flappy-y)
+       (> (+ gap-top pillar-gap)
+          (+ flappy-y flappy-height))))
+
+(defn bottom-collision? [{:keys [flappy-y]}]
+  (>= flappy-y (- bottom-y flappy-height)))
+
+(defn collision? [{:keys [pillar-list] :as st}]
+  (if (some #(or (and (in-pillar? %)
+                      (not (in-pillar-gap? st %)))
+                 (bottom-collision? st)) pillar-list)
+    (assoc st :timer-running false)
+    st))
+
+(defn new-pillar [cur-time pos-x]
+  {:start-time cur-time
+   :pos-x      pos-x
+   :cur-x      pos-x
+   :gap-top    (+ 60 (rand-int (- bottom-y 120 pillar-gap)))})
+
+(defn update-pillars [{:keys [pillar-list cur-time] :as st}]
+  (let [pillars-with-pos (map #(assoc % :cur-x (curr-pillar-pos cur-time %)) pillar-list)
+        pillars-in-world (sort-by
+                          :cur-x
+                          (filter #(> (:cur-x %) (- pillar-width)) pillars-with-pos))]
+    (assoc st
+      :pillar-list
+      (if (< (count pillars-in-world) 3)
+        (conj pillars-in-world
+              (new-pillar
+               cur-time
+               (+ pillar-spacing
+                  (:cur-x (last pillars-in-world)))))
+        pillars-in-world))))
+
+(defn sine-wave [st]
+  (assoc st
+    :flappy-y
+    (+ start-y
+       (* 80 (.sin js/Math (/ (:time-delta st)
+                              200))))))
+
+(defn update-flappy [{:keys [time-delta initial-vel flappy-y jump-count] :as st}]
+  (if (pos? jump-count)
+    (let [cur-vel (- initial-vel (* time-delta gravity))
+          new-y   (- flappy-y cur-vel)
+          new-y   (if (> new-y (- bottom-y flappy-height))
+                    (- bottom-y flappy-height)
+                    new-y)]
+      (assoc st
+        :flappy-y new-y))
+    (sine-wave st)))
+
+(defn score [{:keys [cur-time start-time] :as st}]
+  (let [score (- (.abs js/Math (floor (/ (- (* (- cur-time start-time) horiz-vel) 544)
+                               pillar-spacing)))
+                 4)]
+  (assoc st :score (if (neg? score) 0 score))))
+
+(defn time-update [timestamp state]
+  (-> state
+      (assoc
+          :cur-time timestamp
+          :time-delta (- timestamp (:flappy-start-time state)))
+      update-flappy
+      #_update-pillars
+      #_collision?
+      score))
+
+(defn jump [{:keys [cur-time jump-count] :as state}]
+  (-> state
+      (assoc
+          :jump-count (inc jump-count)
+          :flappy-start-time cur-time
+          :initial-vel jump-vel)))
+
+;; derivatives
+
+(defn border [{:keys [cur-time] :as state}]
+  (-> state
+      (assoc :border-pos (mod (translate 0 horiz-vel cur-time) 23))))
+
+(defn pillar-offset [{:keys [gap-top] :as p}]
+  (assoc p
+    :upper-height gap-top
+    :lower-height (- bottom-y gap-top pillar-gap)))
+
+(defn pillar-offsets [state]
+  (update-in state [:pillar-list]
+             (fn [pillar-list]
+               (map pillar-offset pillar-list))))
+
+(defn world [state]
+  (-> state
+      border
+      pillar-offsets))
+
+(defn px [n] (str n "px"))
+
+(defn pillar [{:keys [cur-x pos-x upper-height lower-height]}]
+  [:div.pillars {:key pos-x}
+   [:div.pillar.pillar-upper {:style {:left (px cur-x)
+                                       :height upper-height}}]
+   [:div.pillar.pillar-lower {:style {:left (px cur-x)
+                                       :height lower-height}}]])
