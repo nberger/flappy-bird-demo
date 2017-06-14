c97788c86372cfef54a414164d4efebd33f55db7
Nicolas Berger <nicoberger@gmail.com>
2017-06-14 01:02:21 -0300

    Extract smaller components

--- src/flappy_bird_demo/subs.cljs
+++ src/flappy_bird_demo/subs.cljs
@@ -9,6 +9,36 @@
       db))
 
   (reg-sub
+    :border-pos
+    (fn [db _]
+      (:border-pos db)))
+
+  (reg-sub
+    :timer-running
+    (fn [db _]
+      (:timer-running db)))
+
+  (reg-sub
+    :paused
+    (fn [db _]
+      (:paused db)))
+
+  (reg-sub
+    :score
+    (fn [db _]
+      (:score db)))
+
+  (reg-sub
+    :flappy-y
+    (fn [db _]
+      (:flappy-y db)))
+
+  (reg-sub
+    :jump-count
+    (fn [db _]
+      (:jump-count db)))
+
+  (reg-sub
     :pillar-list
     (fn [db _]
       (:pillar-list db))))
--- src/flappy_bird_demo/views.cljs
+++ src/flappy_bird_demo/views.cljs
@@ -11,26 +11,38 @@
    [:div.pillar.pillar-lower {:style {:left (px cur-x)
                                        :height lower-height}}]])
 
+(defn border []
+  (let [border-pos @(subscribe [:border-pos])]
+    [:div.scrolling-border {:style {:background-position-x (px border-pos)}}]))
+
+(defn flappy-button []
+  (let [jump-count @(subscribe [:jump-count])
+        paused @(subscribe [:paused])
+        timer-running @(subscribe [:timer-running])]
+    (cond
+      (not timer-running)
+      [:button.start-button {:on-click #(dispatch [:start-game])}
+       (if (< 1 jump-count) "RESTART" "START")]
+
+      paused
+      [:button.start-button {:on-click #(dispatch [:toggle-pause])}
+       "PAUSED"])))
+
+(defn flappy []
+  (let [flappy-y @(subscribe [:flappy-y])]
+    [:div.flappy {:style {:top (px flappy-y)}}]))
+
 (defn main []
   (let [pillar-list @(subscribe [:pillar-list])
-        {:keys [score cur-time jump-count
-                timer-running border-pos
-                flappy-y paused]} @(subscribe [:db])]
+        score @(subscribe [:score])]
     [:div.board {:on-mouse-down (fn [e]
                                   (dispatch [:jump])
                                   (.preventDefault e))}
      [:h1.score score]
-     (cond
-       (not timer-running)
-       [:button.start-button {:on-click #(dispatch [:start-game])}
-        (if (< 1 jump-count) "RESTART" "START")]
-
-       paused
-       [:button.start-button {:on-click #(dispatch [:toggle-pause])}
-        "PAUSED"])
+     [flappy-button]
      [:div
       (for [p pillar-list]
         ^{:key (:key p)}
         [pillar p])]
-     [:div.flappy {:style {:top (px flappy-y)}}]
-     [:div.scrolling-border {:style {:background-position-x (px border-pos)}}]]))
+     [flappy]
+     [border]]))
